b32ede9371384e4d5b4db7cfeb4211ef0cb6d5f2
prometheapelagus-cloud <promethea.pelagus@gmail.com>
2026-07-04 21:40:36 -0300

    feat: export Usuario model

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,6 @@
+from app.models.evento import Evento
+from app.models.cliente import Cliente
+from app.models.pedido import Pedido
+from app.models.usuario import Usuario, TipoUsuario
+
+__all__ = ["Evento", "Cliente", "Pedido", "Usuario", "TipoUsuario"]
